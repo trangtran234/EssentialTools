f18f03705fc9376c43d28b48187145f33810d253
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-10-19 17:26:40 +0700

    Practicing and working with: Dependency Injection - Ninject

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBlindings();
+        }
+
+        private void AddBlindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
