4001039a6ea994e35882e73b1a154713bafc4778
trangtran234 <tn-trang.tran@outlook.com.vn>
2018-10-22 15:04:47 +0700

    Dependency Injection: WithConstructorArgument

--- EssentialTools/Infrastructure/NinjectDependencyResolver.cs
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -21,7 +21,7 @@ namespace EssentialTools.Infrastructure
         private void AddBlindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
-            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithPropertyValue("DiscountSize", 50M);
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
         }
 
         public object GetService(Type serviceType)
--- EssentialTools/Models/Discount.cs
+++ EssentialTools/Models/Discount.cs
@@ -12,10 +12,14 @@ namespace EssentialTools.Models
 
     public class DefaultDiscountHelper : IDiscountHelper
     {
-        public decimal DiscountSize { get; set; }
+        public decimal discountSize;
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
         public decimal ApplyDiscount(decimal totalParam)
         {
-            return (totalParam - (DiscountSize / 100m * totalParam));
+            return (totalParam - (discountSize / 100m * totalParam));
         }
     }
 }
